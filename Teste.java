27a70864f5b3a5094f3e72bb5a6647f1ea2258f9
RogerioGNunes <rg.nunes2013@gmail.com>
2021-11-18 19:34:38 +0000

    classes abstratas

---
+++ Teste.java
@@ -0,0 +1,13 @@
+public class Teste {
+
+    public static void main(String[]args){
+
+        ContaEspecializada contaEspecializada=new ContaEspecializada();
+//Conta
+        contaEspecializada.setSaldo(25000);
+//ContaEspecializada
+        contaEspecializada.imprimeExtrato();
+
+    }
+    
+}
